f2b89f192ebd1ff0f34931b12086b0bcaba20aca
aidengandhi <155774983+aidengandhi@users.noreply.github.com>
2024-02-13 20:25:56 -0800

    Update java12.js

--- M05/java4/java12.js
+++ M05/java4/java12.js
@@ -1,22 +1,22 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+// Ask the user for a number from 0-10
+var userInput = prompt("Enter a number from 0 to 10:");
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
+// Validate the input (optional)
+var table = parseInt(userInput);
+if (isNaN(table) || table < 0 || table > 10) {
+  alert("Invalid input. Please enter a number from 0 to 10.");
 } else {
-  // Do multiplication
+  // Initialize the variable msg for the heading
+  var msg = '<h2>Multiplication Table for ' + table + '</h2>';
+
+  // Show the multiplication table
+  var i = 1; // Set counter to 1
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
-// Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
+  // Write the message into the page
+  var el = document.getElementById('blackboard');
+  el.innerHTML = msg;
+}
