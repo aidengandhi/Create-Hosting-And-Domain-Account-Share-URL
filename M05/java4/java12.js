3732f04b6668747d1895d1c190f61c16e8d2b3a2
aidengandhi <155774983+aidengandhi@users.noreply.github.com>
2024-02-15 16:06:02 -0800

    Update java12.js

--- M05/java4/java12.js
+++ M05/java4/java12.js
@@ -1,22 +1,19 @@
-// Ask the user for a number from 0-10
-var userInput = prompt("Enter a number from 0 to 10:");
+function showMultiplicationTable() {
+      var userInput = document.getElementById('userInput').value;
+      var table = parseInt(userInput);
 
-// Validate the input (optional)
-var table = parseInt(userInput);
-if (isNaN(table) || table < 0 || table > 10) {
-  alert("Invalid input. Please enter a number from 0 to 10.");
-} else {
-  // Initialize the variable msg for the heading
-  var msg = '<h2>Multiplication Table for ' + table + '</h2>';
+      if (isNaN(table) || table < 0 || table > 10) {
+        alert("Invalid input. Please enter a number from 0 to 10.");
+      } else {
+        var msg = '<h2>Multiplication Table for ' + table + '</h2>';
+        var i = 1;
 
-  // Show the multiplication table
-  var i = 1; // Set counter to 1
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+        while (i < 11) {
+          msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+          i++;
+        }
 
-  // Write the message into the page
-  var el = document.getElementById('blackboard');
-  el.innerHTML = msg;
-}
+        var el = document.getElementById('blackboard');
+        el.innerHTML = msg;
+      }
+    }
